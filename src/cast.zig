51f2ad80e542af50d5b64c732586fbf31de5a150
ikouchiha47 <princeofpersiaa3.ag@gmail.com>
2024-06-11 08:28:22 +0530

    feat!: updates to use zig 0.13

--- src/cast.zig
+++ src/cast.zig
@@ -5,19 +5,20 @@ pub fn Cast(comptime T: type) type {
     return struct {
         /// Cast the pointer to a Zig struct to a pointer to a C struct
         pub inline fn toGTK(ptr: anytype) ?*T.GTK {
-            return @ptrCast(?*T.GTK, ptr);
+            return @ptrCast(ptr);
         }
         /// Cast the `const` pointer to a Zig struct to a `const` pointer to a C struct
         pub inline fn toConstGTK(ptr: anytype) ?*const T.GTK {
-            return @ptrCast(?*const T.GTK, ptr);
+            return @ptrCast(ptr);
         }
         /// Cast the pointer to a C struct to a pointer to a Zig struct
         pub inline fn fromGTK(ptr: anytype) ?*T {
-            return @ptrCast(?*T, ptr);
+            return @ptrCast(ptr);
         }
         /// Cast the `const` pointer to a C struct to a `const` pointer to a Zig struct
         pub inline fn fromConstGTK(ptr: anytype) ?*const T {
-            return @ptrCast(?*const T, ptr);
+            // return @ptrCast(?*const T, ptr);
+            return @ptrCast(ptr);
         }
     };
 }
